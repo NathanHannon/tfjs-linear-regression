5081698178b0cd073fc2ae7490d14b567abc251c
Nathan Hannon <nathan@hannon.me>
2021-05-27 12:10:18 -0400

    Initial commit

---
+++ tensorflow.js
@@ -0,0 +1,9 @@
+async function run() {
+    const houseSalesDataset = tf.data.csv("http://127.0.0.1:5500/kc_house_data.csv");
+    const sampleDataset = houseSalesDataset.take(10);
+    const dataArray = await sampleDataset.toArray();
+
+    console.log(dataArray);
+}
+
+run();
